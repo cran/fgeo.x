e9ca110e4936f1038004c4f8c3d13117e316848c
Mauro Lepore <maurolepore@gmail.com>
2019-06-07 20:10:04 +0000

    version 1.1.4

--- NEWS.md
+++ NEWS.md
@@ -2,28 +2,10 @@
 
 * Work in progress.
 
-# fgeo.x 1.1.3 ([GitHub](https://github.com/forestgeo/fgeo.x/releases))
-
-* `download_data()` is now memoised.
-* New `destfile` to `download_data() allows downloading data.
-
-# fgeo.x 1.1.2 ([GitHub](https://github.com/forestgeo/fgeo.x/releases) and [drat](https://forestgeo.github.io/drat/))
+# fgeo.x 1.1.4
 
 * Maintenance release.
 
-# fgeo.x 1.1.1 ([GitHub](https://github.com/forestgeo/fgeo.x/releases))
+# fgeo.x 1.1.3
 
-_fgeo.x_ now requires R >= 3.2.
-
-# fgeo.x 1.1.0 ([GitHub](https://github.com/forestgeo/fgeo.x/releases))
-
-* Meet CRAN standards.
-* First argument of most visible functions are now named following tidyverse principles (<http://bit.ly/2TfDcfX>).
-
-# fgeo.x 1.0.1 ([GitHub](https://github.com/forestgeo/fgeo.x/releases))
-
-* Review documentation.
-
-# fgeo.x 1.0.0 ([GitHub](https://github.com/forestgeo/fgeo.x/releases))
-
-* Initial release.
+* First release on CRAN.
--- R/download_data.R
+++ R/download_data.R
@@ -1,17 +1,3 @@
-download_data_impl <- function(x, destfile = NULL) {
-  if (!is.null(destfile)) {
-    return(utils::download.file(data_url(x), destfile = destfile))
-  }
-
-  tmp <- tempfile()
-  utils::download.file(data_url(x), tmp)
-
-  e <- new.env()
-  load(tmp, envir = e)
-
-  e[[x]]
-}
-
 #' Access data stored online.
 #'
 #' @param x A string giving the name of the dataset to download. The name
@@ -24,6 +10,8 @@ download_data_impl <- function(x, destfile = NULL) {
 #' @return A dataset.
 #'
 #' @examples
+#' # Not running CRAN checks (may take longer than the allowed run time limit)
+#' \donttest{
 #' # Defaults to read data
 #' # The first call is memoised
 #' system.time(download_data("unique_id"))
@@ -32,20 +20,33 @@ download_data_impl <- function(x, destfile = NULL) {
 #'
 #' download_data("unique_id")
 #'
-#' \dontrun{
 #' # Can download data to a destination file given by `destfile`
 #' tmp <- tempfile()
 #' download_data("unique_id", destfile = tmp)
 #' load(tmp)
 #' unique_id
-#' }
 #'
 #' available_data <- "https://github.com/forestgeo/fgeo.data/tree/master/data"
 #' if (interactive()) browseURL(available_data)
+#' }
 #' @family datasets
 #' @seealso [utils::download.file()]
 #' @export
-download_data <- memoise::memoise(download_data_impl)
+#' @importFrom memoise memoise
+#' @importFrom utils download.file
+download_data <- memoise(function(x, destfile = NULL) {
+  if (!is.null(destfile)) {
+    return(download.file(data_url(x), destfile = destfile))
+  }
+
+  tmp <- tempfile()
+  download.file(data_url(x), tmp)
+
+  e <- new.env()
+  load(tmp, envir = e)
+
+  e[[x]]
+})
 
 data_url <- function(x) {
   paste0("https://github.com/forestgeo/fgeo.data/raw/master/data/", x, ".rda")
